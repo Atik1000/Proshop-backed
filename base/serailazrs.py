e049b39fa825ad5d57527ab5f830325469406fa7
Atik1000 <foysalkazi45@gmail.com>
2021-05-28 22:33:24 +0600

    auth system added

---
+++ base/serailazrs.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import *
+
+class ProductSerializar(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields='__all__'
--- base/views.py
+++ base/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .products import products
+from .models import Product
+from .serailazrs import ProductSerializar
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.settings import api_settings
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] =self.user.email 
+        # if api_settings.UPDATE_LAST_LOGIN:
+        #     update_last_login(None, self.user)
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 @api_view(['GET'])
 def getRoutes(request):
-    routes=[ 
+    routes = [
         '/api/products',
         '/api/products/create/',
         '/api/products/upload/',
@@ -22,14 +42,14 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializar(products, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
-def getProduct(request,pk):
-    product=None
-    for i in products:
-        if i['_id']==pk:
-            product=i 
-            break
-    return Response(product)    
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializers = ProductSerializar(product, many=False)
+    return Response(serializers.data)
